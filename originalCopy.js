f85f2979a111383186244c9152e708a6b15b6958
Anne Hyacinthe <annehyacinthe@Annes-Air.home>
2020-03-08 22:00:12 -0400

    hotels added too

--- originalCopy.js
+++ originalCopy.js
@@ -23,8 +23,8 @@ indexForm.addEventListener('submit', async(e) => {
     // Once the submit eventListener has fired off and they have the minimum budget of 500 all of the data(flight and hotel prices) are then retrieved through the getArrOfTrips and the inputs are used as the parameters, go to generateFlights.js for next step
     let arrOfTrips = await getArrOfTrips(origin,arrivalDate,departureDate,budget)
     //returned from getArrOfTrips is an array that holds objects that are each different trips
-    getCities(arrOfTrips,chooseFlight)
-  function getCities(arrOfData,event){
+    addGrid(arrOfTrips,chooseFlight)
+  function addGrid(arrOfData,event){
     let str = ''
   for(let i = 0;i < arrOfData.length;i++){
 
@@ -43,59 +43,29 @@ indexForm.addEventListener('submit', async(e) => {
   <div class="container"> 
 <div class="row">
   <ul class="collection with-header">
-  <li class="collection-header"><h4>Cities</h4></li>
+  <li class="collection-header"><h4>Flights</h4></li>
   ${str}
   </ul>
   </div>
   </div>`
-  addEvent('button',chooseFlight)
+  addEvent('button',event)
 
 
 }
   function addEvent(classname,callbackFunction){
-    let allElements = document.getElementsByClassName(classname)
-    for(let i = 0;i < allElements.length; i++){
-      allElements[i].addEventListener('click',callbackFunction)
+    let allButtons = document.getElementsByClassName(classname)
+    for(let i = 0;i < allButtons.length; i++){
+      allButtons[i].addEventListener('click',callbackFunction)
     }
   }
- 
   function chooseFlight(e){
     // let str = ''
-    let trip = arrOfTrips[e.toElement.id]
-    let roundFlights = trip['roundTrip']
+    let roundFlights = arrOfTrips[e.toElement.id]['roundTrip']
     // getFlightPage is called in this order with o and 1 because roundFlights[0] is the departure flight and vise versa
     thirdPage()
     function thirdPage(){
     let first = getFlightPage(0)
     let second = getFlightPage(1)
-    function getFlightPage(i){
-        let str = ''
-            let leavingDate = roundFlights[i]['Dates']['OutboundDates'][0]['PartialDate']//Date
-            //this loop is going through the quotes because for each trip there can be a selection of flights that can be taken
-            for(let q = 0; q < roundFlights[i]['Quotes'].length; q++){
-            let isDirect =roundFlights[i]['Quotes'][q]['Direct']//is direct?
-            let price = roundFlights[i]['Quotes'][q]['MinPrice']//price?
-            let id = roundFlights[i]['Quotes'][q]['OutboundLeg']['CarrierIds'][0]
-            let carriers = roundFlights[i]['Carriers']
-            //this loop is used to find the carriers name, I must first grab the carrier id for each flight and then take that ID and go to the carriers object
-              for(let c = 0;c < carriers.length; c++){
-                if(carriers[c]['CarrierId'] === id){
-                  let carrierName = carriers[c]['Name']// name?
-                  str += `
-                  <li class="collection-item toHotel">
-                <span class="title">${carrierName}</span>
-                 <p>$${price} <br>
-                 Is Direct:${isDirect}
-                  </p>
-                <a href="#!" class="secondary-content toHotel"><i class="material-icons">grade</i></a>
-                </li>
-                  `
-                }
-              }
-              
-            }
-            return str
-    }
     document.body.innerHTML = ` 
     <div class="container"> 
     <ul class="collection with-header">
@@ -110,41 +80,52 @@ indexForm.addEventListener('submit', async(e) => {
     </ul>
   </div>
   `
-  addEvent('toHotel',chooseHotel(trip))
-    }
-}
-function chooseHotel(trip){
-  
-    fourthPage()
-    function fourthPage(){
-      let hotels = getHotelPage()
-      function getHotelPage(){
+    function getFlightPage(i){
         let str = ''
-            for(let q = 0; q < trip['hotels'].length; q++){
-            
+            let leavingDate = roundFlights[i]['Dates']['OutboundDates'][0]['PartialDate']//Date
+            //this loop is going through the quotes because for each trip there can be a selection of flights that can be taken
+            for(let q = 0; q < roundFlights[i]['Quotes'].length; q++){
+            let isDirect =roundFlights[i]['Quotes'][q]['Direct']//is direct?
+            let price = roundFlights[i]['Quotes'][q]['MinPrice']//price?
+            let id = roundFlights[i]['Quotes'][q]['OutboundLeg']['CarrierIds'][0]
+            let carriers = roundFlights[i]['Carriers']
+            //this loop is used to find the carriers name, I must first grab the carrier id for each flight and then take that ID and go to the carriers object
+              for(let c = 0;c < carriers.length; c++){
+                if(carriers[c]['CarrierId'] === id){
+                  let carrierName = carriers[c]['Name']// name?
                   str += `
-                  <li class="collection-item">
-                <span class="title">${trip['hotels'][q]['name']}</span>
-                <a href="#!" class="secondary-content toHotel"><i class="material-icons">grade</i></a>
+                  <li class="collection-item avatar">
+                <span class="title">${carrierName}</span>
+                 <p>$${price} <br>
+                 Is Direct:${isDirect}
+                  </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
                 </li>
                   `
                 }
-                return str
               }
-    document.body.innerHTML = ` 
-    <div class="container"> 
-    <ul class="collection with-header">
-    <li class="collection-header"><h4>Hotels</h4></li>
-    <ul class="collection">
-    ${hotels}
-    </ul>
-  </div>
-  `
- }
- }
+              
+            }
+            return str
+    }
   }
-}   
+  
 
+  // <li class="collection-item">
+                  
+  //                   <span class="price">$${price}</span>
+  //                   <p>${carrierName} <br>
+  //                     Is Direct:${isDirect}
+  //                   </p>
+  //                   <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
+                  
+  //               </li>
+    // console.log(e.toElement.id)
+    // arrOfTrips[e.toElement.id]['roundTrip'][0]['Quotes'][0]['OutboundLeg']['CarrierIds'][0]
+    // console.log(,arrOfTrips[e.toElement.id]['roundTrip'][0]['Quotes'][0]['Direct'],
+    // rrOfTrips[e.toElement.id]['roundTrip'][0]['Carriers'])
+  }
+  }
 
-)
+}); 	
 
